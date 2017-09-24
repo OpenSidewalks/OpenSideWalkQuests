2360acf376bb9fe79577b88e36277c540ac80bac
Samantha Gallagher <sjgllghr@gmail.com>
2017-09-23 20:37:01 -0700

    change waste_basket to bin

--- app/src/main/java/de/westnordost/streetcomplete/quests/QuestModule.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/QuestModule.java
@@ -10,36 +10,13 @@ import de.westnordost.streetcomplete.data.QuestType;
 import de.westnordost.streetcomplete.data.QuestTypes;
 import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
 import de.westnordost.streetcomplete.data.osmnotes.OsmNoteQuestType;
-import de.westnordost.streetcomplete.quests.baby_changing_table.AddBabyChangingTable;
-import de.westnordost.streetcomplete.quests.bike_parking_capacity.AddBikeParkingCapacity;
-import de.westnordost.streetcomplete.quests.bike_parking_cover.AddBikeParkingCover;
-import de.westnordost.streetcomplete.quests.building_levels.AddBuildingLevels;
 import de.westnordost.streetcomplete.quests.bus_stop_bench.AddBusStopBench;
 import de.westnordost.streetcomplete.quests.bus_stop_shelter.AddBusStopShelter;
-import de.westnordost.streetcomplete.quests.crossing_type.AddCrossingType;
-import de.westnordost.streetcomplete.quests.fire_hydrant.AddFireHydrantType;
-import de.westnordost.streetcomplete.quests.parking_type.AddParkingType;
 import de.westnordost.streetcomplete.quests.passenger_information_display.AddPassengerInformationDisplay;
-import de.westnordost.streetcomplete.quests.powerpoles_material.AddPowerPolesMaterial;
-import de.westnordost.streetcomplete.quests.orchard_produce.AddOrchardProduce;
-import de.westnordost.streetcomplete.quests.recycling.AddRecyclingType;
 import de.westnordost.streetcomplete.quests.road_name.data.PutRoadNameSuggestionsHandler;
 import de.westnordost.streetcomplete.quests.road_name.data.RoadNameSuggestionsDao;
 import de.westnordost.streetcomplete.quests.tactile_paving.AddTactilePavingBusStop;
-import de.westnordost.streetcomplete.quests.tactile_paving.AddTactilePavingCrosswalk;
-import de.westnordost.streetcomplete.quests.toilet_availability.AddToiletAvailability;
-import de.westnordost.streetcomplete.quests.toilets_fee.AddToiletsFee;
-import de.westnordost.streetcomplete.quests.housenumber.AddHousenumber;
-import de.westnordost.streetcomplete.quests.max_speed.AddMaxSpeed;
-import de.westnordost.streetcomplete.quests.opening_hours.AddOpeningHours;
-import de.westnordost.streetcomplete.quests.road_name.AddRoadName;
-import de.westnordost.streetcomplete.quests.road_surface.AddRoadSurface;
-import de.westnordost.streetcomplete.quests.roof_shape.AddRoofShape;
-import de.westnordost.streetcomplete.quests.sport.AddSport;
-import de.westnordost.streetcomplete.quests.waste_basket.AddWasteBasket;
-import de.westnordost.streetcomplete.quests.way_lit.AddWayLit;
-import de.westnordost.streetcomplete.quests.wheelchair_access.AddWheelChairAccessPublicTransport;
-import de.westnordost.streetcomplete.quests.wheelchair_access.AddWheelchairAccessBusiness;
+import de.westnordost.streetcomplete.quests.bin.AddBin;
 
 @Module
 public class QuestModule
@@ -52,7 +29,7 @@ public class QuestModule
 		QuestType[] questTypesOrderedByImportance = {
 				// ↓ notes
 				osmNoteQuestType,
-				new AddWasteBasket(o),
+				new AddBin(o),
 				new AddPassengerInformationDisplay(o),
 				new AddBusStopBench(o),
 				new AddBusStopShelter(o),
--- app/src/main/java/de/westnordost/streetcomplete/quests/bin/AddBin.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bin/AddBin.java
@@ -1,4 +1,4 @@
-package de.westnordost.streetcomplete.quests.waste_basket;
+package de.westnordost.streetcomplete.quests.bin;
 
 import android.os.Bundle;
 
@@ -13,13 +13,13 @@ import de.westnordost.streetcomplete.data.osm.download.OverpassMapDataDao;
 import de.westnordost.streetcomplete.quests.AbstractQuestAnswerFragment;
 import de.westnordost.streetcomplete.quests.YesNoQuestAnswerFragment;
 
-public class AddWasteBasket extends SimpleOverpassQuestType
+public class AddBin extends SimpleOverpassQuestType
 {
-    @Inject public AddWasteBasket(OverpassMapDataDao overpassServer) { super(overpassServer); }
+    @Inject public AddBin(OverpassMapDataDao overpassServer) { super(overpassServer); }
 
     @Override protected String getTagFilters()
     {
-        return "nodes with (public_transport=platform or (highway=bus_stop and public_transport!=stop_position)) and !waste_basket";
+        return "nodes with (public_transport=platform or (highway=bus_stop and public_transport!=stop_position)) and !bin";
     }
 
     public AbstractQuestAnswerFragment createForm()
@@ -30,15 +30,15 @@ public class AddWasteBasket extends SimpleOverpassQuestType
     public void applyAnswerTo(Bundle answer, StringMapChangesBuilder changes)
     {
         String yesno = answer.getBoolean(YesNoQuestAnswerFragment.ANSWER) ? "yes" : "no";
-        changes.add("waste_basket", yesno);
+        changes.add("bin", yesno);
     }
 
-    @Override public String getCommitMessage() { return "Add bus stop waste basket"; }
+    @Override public String getCommitMessage() { return "Add bus stop bin"; }
     @Override public int getIcon() { return R.drawable.ic_quest_bus; }
     @Override public int getTitle(Map<String, String> tags)
     {
         boolean hasName = tags.containsKey("name");
-        if(hasName) return R.string.quest_wasteBasket_name_title;
-        else        return R.string.quest_wasteBasket_title;
+        if(hasName) return R.string.quest_bin_name_title;
+        else        return R.string.quest_bin_title;
     }
 }
